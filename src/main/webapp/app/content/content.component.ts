fbc248e44ce86d70a5e5a6af804a2b88fa1cff97
Flavio.Borsatto <Flavio.Borsatto@POAN040101034.poa01.local>
2019-01-05 14:21:30 -0200

    Ajustes

--- src/main/webapp/app/content/content.component.ts
+++ src/main/webapp/app/content/content.component.ts
@@ -19,4 +19,8 @@ export class ContentComponent implements OnInit {
   gotoCadastroClientes() {
     this.router.navigate(['cadastro']);
    }
+
+   gotoConsultaPropostas() {
+    this.router.navigate(['consulta']);
+   }
 }
